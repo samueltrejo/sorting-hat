4d4065d7d312dbcd5d9eca103ee491ee75f51720
Samuel Trejo <samuel.trejo@s.tcatnashville.edu>
2019-03-23 13:57:29 -0500

    add error checking for blank input

--- main.js
+++ main.js
@@ -28,10 +28,22 @@ const randomHouse = () => {
 const printStudent = () => {
   const newStudent = {};
   newStudent.name = document.getElementById('name').value
-  newStudent.house = randomHouse()
-  document.getElementById
-  students.push(newStudent);
-  domStringBuilder(students);
+  if(newStudent.name.length === 0) {
+    const alert = `
+      <div class="alert alert-warning alert-dismissible fade show" role="alert">
+        <strong>Whoops!</strong> Looks like you forgot to enter your name.
+        <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>`
+    printToDom('alert-container', alert)
+  } else {
+    newStudent.house = randomHouse()
+    document.getElementById
+    students.push(newStudent);
+    domStringBuilder(students);
+    $('#form-modal').modal('hide');
+  }
 
   document.getElementById('name').value = '';
 }
@@ -62,6 +74,12 @@ const domStringBuilder = (array) => {
   })
 }
 
+const hideAlert = (event) => {
+  if(event.target.id !== 'sort') {
+    $('.alert').alert('close');
+  }
+}
+
 const printToDom = (divId, textToPrint) => {
   const selectedDiv = document.getElementById(divId);
   selectedDiv.innerHTML = textToPrint;
@@ -73,6 +91,8 @@ const expelEventListeners = (element) => {
 
 const eventListeners = () => {
   document.getElementById('sort').addEventListener('click', printStudent);
+  document.getElementById('modal-x').addEventListener('click', hideAlert);
+  document.getElementById('form-modal').addEventListener('click', hideAlert);
 }
 
 const init = () => {
