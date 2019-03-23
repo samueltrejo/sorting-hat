a3bf4e753a604ee0acb4408fbd69c9fdf097cbe9
Samuel <mrto0ns@live.com>
2019-03-22 20:55:24 -0500

    added printstudent functionality

--- main.js
+++ main.js
@@ -1,12 +1,49 @@
+let students = [
+  {
+    name: 'Joe'
+  },
+  {
+    name: 'Bob'
+  },
+  {
+    name: 'Bill'
+  },
+  {
+    name: 'Doug'
+  }
+];
 
+const printStudent = () => {
+  const newStudent = {};
+  newStudent.name = document.getElementById('name').value
+  students.push(newStudent);
+  console.log(newStudent, students)
+  domStringBuilder(students);
+}
 
-const pintToDom = (divId, textToPrint) => {
+const domStringBuilder = (array) => {
+  let domString = '';
+  array.forEach((item) => {
+    domString += `<div class="card">`;
+    domString += `<h3>${item.name}</h3>`;
+    domString += `<button id=${item.name}>Expel</button>`
+    domString += `</div>`;
+  })
+  printToDom('sorting-hat', domString);
+}
+
+const printToDom = (divId, textToPrint) => {
   const selectedDiv = document.getElementById(divId);
   selectedDiv.innerHTML = textToPrint;
 }
 
-const init = () => {
+const eventListeners = () => {
+  document.getElementById('sort').addEventListener('click', printStudent);
+}
 
+const init = () => {
+  domStringBuilder(students);
+  eventListeners();
 }
 
 init()
